11948144cd7f723d25d97c73fdd4b90ae57e25bb
teomanakgnn <teomanakgn84@gmail.com>
2026-01-18 21:03:41 +0000

    daily push sun1801.4

--- services/selenium_scraper.py
+++ services/selenium_scraper.py
@@ -6,7 +6,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
-from os import os
+import os
 
 def get_driver():
     """
