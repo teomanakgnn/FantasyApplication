2cc8c76eb0fb16018188fa56003fd74456112acf
teomanakgnn <teomanakgn84@gmail.com>
2026-01-29 15:09:58 +0000

    header change

--- components/header.py
+++ components/header.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 def render_header():
-    st.markdown("## NBA Player Performance Index")
+    st.markdown("## NBA Player Stats & Analysis")
     st.markdown(
         "<span style='color:#9AA0A6'>"
         "Box score–based daily performance evaluation"
